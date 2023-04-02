8f94a8d716d2b9f816c9c917d0d7daf3966c06e3
zzaeneys <zkk054@naver.com>
2023-04-02 19:42:31 +0900

    Initial commit

---
+++ src/FourArithmetic.java
@@ -0,0 +1,10 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 2;
+        System.out.println("a+b=" + a+b);
+        System.out.println("a-b=" + (a-b));
+        System.out.println("a*b=" + a*b);
+        System.out.println("a/b=" + a/b);
+    }
+}
---
+++ src/Operator.java
@@ -0,0 +1,6 @@
+public class Operator {
+    public static void main(String[] args) {
+        int koreanScore = 87;
+        int englishScore = 98;
+    }
+}
