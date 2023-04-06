5bec565834065faa20fb95d77efd19f0fa3bf3ae
zzaeneys <zkk054@naver.com>
2023-04-06 20:10:27 +0900

    commit

---
+++ src/FindAPI.java
@@ -0,0 +1,12 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        // AAA b;
+        a = 10;
+        System.out.println("a = " + a);
+        // "APPLE" 문자열 저장하고 출력
+        String s;
+
+
+    }
+}
