941814ed55ef1556b7b3df3d28ae7de74a94d67b
zzaeneys <zkk054@naver.com>
2023-04-18 11:43:35 +0900

    commit

---
+++ src/ArrayBasicTest.java
@@ -0,0 +1,12 @@
+public class ArrayBasicTest {
+    public static void main(String[] args) {
+        // Q. 정수 3개를 저장 할 변수를 선언하고 모두 10을 저장하시오.
+        // 데이터의 수가 늘어나면
+        int a, b, c;
+        a=10;
+        b=10;
+        c=10;
+        int sum = a+b+c;
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ src/ArrayExam.java
@@ -0,0 +1,7 @@
+public class ArrayExam {
+    public static void main(String[] args) {
+        char[] c = {'A', 'P', 'P', 'L', 'E'};
+            System.out.print(c);
+
+    }
+}
